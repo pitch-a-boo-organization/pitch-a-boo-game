98e1f4dc229508557d5f9ac4e2820bfc31b408b5
ThiagoHBA <thiagohhab@gmail.com>
2023-06-14 08:21:03 -0300

    first commit

---
+++ pitch-a-boo-game/Application/pitch_a_boo_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  pitch_a_boo_gameApp.swift
+//  pitch-a-boo-game
+//
+//  Created by Thiago Henrique on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct pitch_a_boo_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pitch-a-boo-game/Source/Presentation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pitch-a-boo-game
+//
+//  Created by Thiago Henrique on 14/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
