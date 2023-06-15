8582e8ca34d41f0fe1b91b57504e0a2712a4b278
ThiagoHBA <thiagohhab@gmail.com>
2023-06-15 11:18:11 -0300

    getting device hostname and connecting in client

--- pitch-a-boo-game/Source/Presentation/ExampleView/ContentView.swift
+++ pitch-a-boo-game/Source/Presentation/ExampleView/ContentView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var viewModel = ViewModel()
+    @State var serverName = ""
+    @State var hostname = ""
     
     var body: some View {
         VStack {
             #if os(macOS)
+            Text("Connect to hostname: \(hostname)")
+                .font(.title)
+                .padding([.bottom], 50)
+            
             Button("Start Server") {
                 let server = try! PitchABooWebSocketServer(port: 8080)
                 server.startServer { error in
                     if let error = error {
                         print("Error! \(error)")
-                    } else {
-                        print("Initializing Server")
+                        return
                     }
+                    hostname = server.getDeviceHostname() ?? ""
                 }
             }
             #else
@@ -32,11 +38,18 @@ struct ContentView: View {
                 .font(.title3)
                 .padding([.bottom], 150)
             
+            TextField(text: $serverName, label: {
+                Text("Server Name")
+                    .font(.title3)
+            })
+            
             Button("Start Listening") {
                 let client = PitchABooSocketClient.shared
+                client.defineServerURL(hostname: serverName)
                 client.delegate = viewModel
                 client.subscribeToService()
             }
+            .disabled(serverName == "")
             #endif
         }
         .padding()
