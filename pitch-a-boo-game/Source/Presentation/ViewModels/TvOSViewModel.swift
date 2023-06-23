68247ec09b9e049247633a6869a68bc87cb72b19
ThiagoHBA <thiagohhab@gmail.com>
2023-06-23 15:24:13 -0300

    adding more outputs in viewmodel

--- pitch-a-boo-game/Source/Presentation/ViewModels/TvOSViewModel.swift
+++ pitch-a-boo-game/Source/Presentation/ViewModels/TvOSViewModel.swift
@@ -11,10 +11,19 @@ import PitchABooServer
 class TvOSViewModel: ObservableObject {
     @Published var server = try! PitchABooWebSocketServer(port: 8080)
     @Published var players: [PitchABooServer.Player] = []
+    
     let client = PitchABooSocketClient.shared
 }
 
 extension TvOSViewModel: PitchABooServer.ServerOutputs {
+    func didDefineSellingPlayer(_ player: PitchABooServer.Player) {
+        //Define server player
+    }
+    
+    func inningEnd(players: [PitchABooServer.Player], gameEnded: Bool, result: PitchABooServer.SaleResult) {
+        //Define inning end
+    }
+    
     func didConectPlayer(players: [PitchABooServer.Player]) {
         DispatchQueue.main.async { [weak self] in
             self?.players = players
