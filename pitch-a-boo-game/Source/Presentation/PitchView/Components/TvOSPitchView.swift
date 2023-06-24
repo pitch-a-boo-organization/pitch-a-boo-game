8d05603190cc8dfd8a9d0f56cd6ce22ac2dcf48c
JoanWilson <joanwmoliveira@gmail.com>
2023-06-24 17:00:37 -0300

    refactor: change bidPitchView color

--- pitch-a-boo-game/Source/Presentation/PitchView/Components/TvOSPitchView.swift
+++ pitch-a-boo-game/Source/Presentation/PitchView/Components/TvOSPitchView.swift
@@ -11,11 +11,18 @@ import SwiftUI
 struct TvOSPitchView: View {
     @EnvironmentObject var pitchViewModel: TvOSViewModel
     @State var player: Int = 01
-
+    
     @State var navigateToView = false
     var body: some View {
+        ZStack {
+            Image("GhostBackground")
+                .resizable()
+                .scaledToFill()
+                .background(Color("PitchBackground"))
+                .ignoresSafeArea(.all)
+            
             VStack(spacing: 50) {
-                Counter(countdown: 45,
+                Counter(countdown: 4500,
                     timersUp: {
                         navigateToView = true
                     }
@@ -24,12 +31,22 @@ struct TvOSPitchView: View {
                     ReviewItemView()
                 }
                 Spacer()
-                if let player = pitchViewModel.sellingPlayer {
-                    Text("Player \(player.name) is presenting...")
+                
+                    Text("Player Fulano is presenting...")
                         .font(.title)
-                }
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.black)
+                
+//                if let player = pitchViewModel.sellingPlayer {
+//                    Text("Player \(player.name) is presenting...")
+//                        .font(.title)
+//                }
                 Spacer()
             }
+            
+            
+        }
     }
 }
 #endif
