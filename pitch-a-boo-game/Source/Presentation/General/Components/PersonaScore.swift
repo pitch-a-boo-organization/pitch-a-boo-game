9eb01126050fb0e87f83f93557ad073edb9e149d
Cicero Nascimento <antoniociceero@gmail.com>
2023-06-26 16:32:37 -0300

    feat: size cards fixed, personas endgame added

--- pitch-a-boo-game/Source/Presentation/General/Components/PersonaScore.swift
+++ pitch-a-boo-game/Source/Presentation/General/Components/PersonaScore.swift
@@ -4,17 +4,34 @@
 //
 //  Created by Cicero Nascimento on 26/06/23.
 //
-
+#if os(tvOS)
 import SwiftUI
+import PitchABooServer
 
 struct PersonaScore: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    let imageCoin = "BoneCoin"
+    let tombstone = "whiteTombstone"
+    let player: PitchABooServer.Player
 
-struct PersonaScore_Previews: PreviewProvider {
-    static var previews: some View {
-        PersonaScore()
+    var body: some View {
+        VStack {
+            Text(player.name)
+                .foregroundColor(.black)
+                .font(.title3)
+            Image(player.persona.name)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 87,maxHeight: 87)
+            HStack(spacing: 6) {
+                Image(imageCoin)
+                    .resizable()
+                    .frame(width: 58.25, height: 53.67)
+                Text("\(player.bones)")
+                    .bold()
+                    .foregroundColor(.black)
+                    .font(.title3)
+            }
+        }
     }
 }
+#endif
