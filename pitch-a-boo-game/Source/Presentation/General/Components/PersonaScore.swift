a0a38950ba205d27feee674453264ec52f92fffc
Cicero Nascimento <antoniociceero@gmail.com>
2023-06-26 16:31:31 -0300

    feat: size cards fixed, personas endgame

---
+++ pitch-a-boo-game/Source/Presentation/General/Components/PersonaScore.swift
@@ -0,0 +1,20 @@
+//
+//  PersonaScore.swift
+//  pitch-a-boo-game
+//
+//  Created by Cicero Nascimento on 26/06/23.
+//
+
+import SwiftUI
+
+struct PersonaScore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonaScore_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonaScore()
+    }
+}
